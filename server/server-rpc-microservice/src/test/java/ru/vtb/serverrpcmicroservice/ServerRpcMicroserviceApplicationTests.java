7f7a0767a06df56bea50ea0795c568e957d4e665
d.shishlov <dima.shishlov89@mail.ru>
2022-10-06 00:03:51 +0300

    перепаковал

---
+++ server/server-rpc-microservice/src/test/java/ru/vtb/serverrpcmicroservice/ServerRpcMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vtb.serverrpcmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRpcMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
