6363f9d36b7a5e1656401adc75c5005dd80dd59f
phoekoby <phoekoby@mail.ru>
2022-10-05 19:29:34 +0300

    init commit

---
+++ server-rpc-microservice/src/test/java/ru/vtb/phoekoby/serverrpcmicroservice/ServerRpcMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vtb.phoekoby.serverrpcmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRpcMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
