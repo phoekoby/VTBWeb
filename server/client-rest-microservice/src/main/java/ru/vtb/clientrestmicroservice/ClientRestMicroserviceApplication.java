d5afd2245c6d2cdd59cf8422cd126937b6cca91d
Vladimir <vladimir.kovalenko@combridge-itc.de>
2022-10-07 12:37:17 +0300

    Перенесен Клиент в папку src/webapp/

--- server/client-rest-microservice/src/main/java/ru/vtb/clientrestmicroservice/ClientRestMicroserviceApplication.java
+++ server/client-rest-microservice/src/main/java/ru/vtb/clientrestmicroservice/ClientRestMicroserviceApplication.java
@@ -2,10 +2,12 @@ package ru.vtb.clientrestmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"ru.vtb.clientrestmicroservice", "ru.vtb.integrationmodule"})
+@RefreshScope
 public class ClientRestMicroserviceApplication {
 
     public static void main(String[] args) {
--- server/server-rpc-microservice/src/main/java/ru/vtb/serverrpcmicroservice/ServerRpcMicroserviceApplication.java
+++ server/server-rpc-microservice/src/main/java/ru/vtb/serverrpcmicroservice/ServerRpcMicroserviceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,8 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(value="ru.vtb.integrationmodule.repo")
 @EntityScan(basePackages = "ru.vtb.integrationmodule.entity")
 @ComponentScan(basePackages = {"ru.vtb.serverrpcmicroservice", "ru.vtb.integrationmodule"})
-@EnableJpaRepositories("ru.vtb.integrationmodule.repo")
-@EntityScan(basePackages = "ru.vtb.integrationmodule.entity")
+//заставить прочитать заново конфигурацию в случае ее изменения
+@RefreshScope
 public class ServerRpcMicroserviceApplication {
 
     public static void main(String[] args) {
