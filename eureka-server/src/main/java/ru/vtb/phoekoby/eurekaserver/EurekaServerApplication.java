40b7be36f6a8a587c1a3331f733bbb9b7dd5f0ce
Vladimir <vladimir.kovalenko@combridge-itc.de>
2022-10-07 20:30:02 +0300

    Добавлен EurekaServer

---
+++ eureka-server/src/main/java/ru/vtb/phoekoby/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package ru.vtb.phoekoby.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@RefreshScope
+@EnableEurekaServer
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
