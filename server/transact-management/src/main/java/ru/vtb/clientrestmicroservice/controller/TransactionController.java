acfde0b6baf0464d235db7ad8e2d7a8519c0ca4e
d.shishlov <dima.shishlov89@mail.ru>
2022-10-09 00:58:22 +0300

    Добавил метод по транзакции

--- server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/controller/TransactionController.java
+++ server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/controller/TransactionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import ru.vtb.clientrestmicroservice.dto.input.ExchangeDto;
 import ru.vtb.clientrestmicroservice.dto.input.PurchaseDto;
 import ru.vtb.clientrestmicroservice.dto.input.TransferDto;
 import ru.vtb.clientrestmicroservice.dto.output.OutTransactionDto;
@@ -19,6 +20,11 @@ public class TransactionController {
     @PostMapping("/transfer")
     public ResponseEntity<OutTransferDto> makeTransfer(@RequestBody TransferDto transferDto){
         return ResponseEntity.ok(transactionService.doTransfer(transferDto));
+    }
+
+    @PostMapping("/exchange")
+    public ResponseEntity<OutTransactionDto> makeExchange(@RequestBody ExchangeDto exchangeDto){
+        return ResponseEntity.ok(transactionService.doExchange(exchangeDto));
     }
 
     @PostMapping("/purchase")
