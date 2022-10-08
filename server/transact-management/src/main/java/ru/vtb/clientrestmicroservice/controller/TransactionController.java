d403335d448496246ced487b6c82641a498ede59
d.shishlov <dima.shishlov89@mail.ru>
2022-10-08 11:18:17 +0300

    Сделал метод для проверки транзакции

--- server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/controller/TransactionController.java
+++ server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/controller/TransactionController.java
@@ -3,9 +3,7 @@ package ru.vtb.clientrestmicroservice.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.vtb.clientrestmicroservice.dto.input.TransferDto;
 import ru.vtb.clientrestmicroservice.dto.output.OutTransferDto;
 import ru.vtb.clientrestmicroservice.service.TransactionService;
@@ -19,6 +17,11 @@ public class TransactionController {
     @PostMapping("/transfer")
     public ResponseEntity<OutTransferDto> makeTransfer(@RequestBody TransferDto transferDto){
         return ResponseEntity.ok(transactionService.doTransfer(transferDto));
+    }
+
+    @GetMapping("/{hash}")
+    public ResponseEntity<String> getTransactionInfo(@PathVariable String hash){
+        return ResponseEntity.ok(transactionService.getStatus(hash));
     }
 
 }
