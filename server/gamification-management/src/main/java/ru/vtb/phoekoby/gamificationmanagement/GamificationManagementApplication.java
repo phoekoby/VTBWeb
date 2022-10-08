e1d6f83682d3088b9fa170af500707fd4f236938
Vladimir <vladimir.kovalenko@combridge-itc.de>
2022-10-09 01:38:02 +0300

    added configuration for new microservice

--- server/gamification-management/src/main/java/ru/vtb/phoekoby/gamificationmanagement/GamificationManagementApplication.java
+++ server/gamification-management/src/main/java/ru/vtb/phoekoby/gamificationmanagement/GamificationManagementApplication.java
@@ -2,8 +2,12 @@ package ru.vtb.phoekoby.gamificationmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@RefreshScope
 public class GamificationManagementApplication {
 
 	public static void main(String[] args) {
