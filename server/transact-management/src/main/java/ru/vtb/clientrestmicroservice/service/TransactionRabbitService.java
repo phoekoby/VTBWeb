faf7e53d2c48a17edad73ba07ef6ebdf144f9674
d.shishlov <dima.shishlov89@mail.ru>
2022-10-08 11:15:33 +0300

    Доделал перевод

--- server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/service/TransactionRabbitService.java
+++ server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/service/TransactionRabbitService.java
@@ -1,6 +1,5 @@
 package ru.vtb.clientrestmicroservice.service;
 
-import org.springframework.stereotype.Service;
 import ru.vtb.clientrestmicroservice.dto.message.TransactionMessageEventDto;
 
 public interface TransactionRabbitService {
