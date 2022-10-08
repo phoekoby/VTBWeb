bd98f266066bf1e04f0d5a03f822ef4021f66fd2
d.shishlov <dima.shishlov89@mail.ru>
2022-10-08 23:52:32 +0300

    Добавил модуль геймификации, почти доделал транзакции

--- server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/service/TransactionService.java
+++ server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/service/TransactionService.java
@@ -1,11 +1,15 @@
 package ru.vtb.clientrestmicroservice.service;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import ru.vtb.clientrestmicroservice.dto.input.ExchangeDto;
+import ru.vtb.clientrestmicroservice.dto.input.PurchaseDto;
 import ru.vtb.clientrestmicroservice.dto.input.TransferDto;
+import ru.vtb.clientrestmicroservice.dto.message.ExchangeMessageEventDto;
+import ru.vtb.clientrestmicroservice.dto.message.PurchaseMessageEventDto;
 import ru.vtb.clientrestmicroservice.dto.message.TransactionMessageEventDto;
-import ru.vtb.clientrestmicroservice.dto.output.OutExchangeDto;
+import ru.vtb.clientrestmicroservice.dto.output.OutTransactionDto;
 import ru.vtb.clientrestmicroservice.dto.output.OutTransferDto;
 import ru.vtb.clientrestmicroservice.dto.output.TransactionDto;
 
@@ -14,10 +18,12 @@ import java.util.List;
 @Service
 public interface TransactionService {
     OutTransferDto doTransfer(TransferDto transferDto);
-    OutExchangeDto doExchange(ExchangeDto exchangeDto);
+    OutTransactionDto doExchange(ExchangeDto exchangeDto);
+    OutTransactionDto doPurchase(PurchaseDto purchaseDto);
     String getStatus(String hash);
-    void analyseTransaction(TransactionMessageEventDto transactionMessageEventDto);
-    void analyseExchange();
+    void checkTransaction(TransactionMessageEventDto transactionMessageEventDto);
+    void checkExchange(ExchangeMessageEventDto exchangeMessageEventDto);
+    void checkPurchase(PurchaseMessageEventDto purchaseMessageEventDto);
     List<TransactionDto> getTransfersByWalletId(Long id, Pageable pageable);
 
 }
--- server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/service/impl/TransactionRabbitServiceImpl.java
+++ server/transact-management/src/main/java/ru/vtb/clientrestmicroservice/service/impl/TransactionRabbitServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.vtb.clientrestmicroservice.service.impl;
 
 import org.springframework.stereotype.Service;
+import ru.vtb.clientrestmicroservice.dto.message.ExchangeMessageEventDto;
+import ru.vtb.clientrestmicroservice.dto.message.PurchaseMessageEventDto;
 import ru.vtb.clientrestmicroservice.dto.message.TransactionMessageEventDto;
 import ru.vtb.clientrestmicroservice.service.TransactionRabbitService;
 
@@ -8,6 +10,16 @@ import ru.vtb.clientrestmicroservice.service.TransactionRabbitService;
 public class TransactionRabbitServiceImpl implements TransactionRabbitService {
     @Override
     public void sendToControl(TransactionMessageEventDto transactionMessageEventDto) {
+
+    }
+
+    @Override
+    public void sendToControl(ExchangeMessageEventDto exchangeMessageEventDto) {
+
+    }
+
+    @Override
+    public void sendToControl(PurchaseMessageEventDto purchaseMessageEventDto) {
 
     }
 }
