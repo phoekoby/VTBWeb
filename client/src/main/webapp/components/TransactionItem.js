fe43ba168c2c9876210bf27e4d8da4fbacd26992
MaksimSamedov <makssamedov36@mail.ru>
2022-10-08 17:42:29 +0300

    Разворачивание транзакций

--- client/src/main/webapp/components/TransactionItem.js
+++ client/src/main/webapp/components/TransactionItem.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Card, Col, Image} from "react-bootstrap";
 import styles from "../styles/TransactionItem.module.css";
-import coin from "../assets/coin.png"
+import wallet from "../assets/wallet.png"
 import {useNavigate} from 'react-router-dom'
 import {TRANSACTIONS_ROUTE} from "../utils/consts";
 import Container from "react-bootstrap/Container";
@@ -9,19 +9,33 @@ import Row from "react-bootstrap/Row";
 
 const TransactionItem = ({transaction}) => {
     const history = useNavigate()
+    console.log('TransactionItem', transaction)
+    const [isOpen, setOpen] = useState(false)
+    const toggle = () => setOpen(!isOpen)
 
     return (
-        <Container className={styles.transactionItem} onClick={() => {}}>
+        <Container className={styles.transactionItem} onClick={toggle}>
             <Row className={styles.info}>
                 <Col xs={3} className={styles.text}>{transaction.hash}</Col>
                 <Col xs={3} className={styles.price}>{transaction.amount} {transaction.currency}</Col>
-                <Col xs={3}>
-                    {transaction.fromId}
-                    {(transaction.direction === 'Incoming') ? ' <- ' : ' -> '}
-                    {transaction.toId}
-                </Col>
+                <Col xs={3} style={{color: 'black'}}>{(transaction.direction === 'Incoming') ? '<-' : '->'}</Col>
                 <Col xs={3}>{transaction.transactionType}</Col>
             </Row>
+            {
+                isOpen && (
+                    <Row className={styles.detail}>
+                        <Col className={styles.detailCol} xs={3}>
+                            <Image src={wallet} />
+                            Ваш кошелёк (№{transaction.fromId})
+                        </Col>
+                        <Col xs={3}></Col>
+                        <Col className={styles.detailCol} xs={3}>
+                            {(transaction.direction === 'Incoming') ? 'Отправитель' : 'Получатель'} (№{transaction.toId})
+                            <Image src={wallet} />
+                        </Col>
+                    </Row>
+                )
+            }
         </Container>
     );
 };
