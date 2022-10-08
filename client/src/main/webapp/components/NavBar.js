cb2d3eb68535396956964b2a9e2c47348a8d0cee
MaksimSamedov <makssamedov36@mail.ru>
2022-10-08 15:33:08 +0300

    Ссылки по центру шапки

--- client/src/main/webapp/components/NavBar.js
+++ client/src/main/webapp/components/NavBar.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {Context} from "../../../index";
 import {Navbar, Nav, Button, Container} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {COURSES_ROUTE, NOTIFICATIONS_ROUTE, SHOP_ROUTE, TRANSACTIONS_ROUTE, USERS_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
 import styles from "../styles/Navbar.module.css";
 
@@ -12,6 +12,12 @@ const NavBar = observer(() => {
         <Navbar className={styles.navbar} bg="dark" variant="dark">
             <Container>
                 <NavLink  to={SHOP_ROUTE} className={styles.logo}>ВТБХАК</NavLink>
+                <Container>
+                    <NavLink to={TRANSACTIONS_ROUTE} className={styles.button}>Транзакции</NavLink>
+                    <NavLink to={COURSES_ROUTE} className={styles.button}>Курсы</NavLink>
+                    <NavLink to={USERS_ROUTE} className={styles.button}>Другие люди</NavLink>
+                    <NavLink to={NOTIFICATIONS_ROUTE} className={styles.button}>Уведомления</NavLink>
+                </Container>
                 {user.isAuth ?
                     <Nav style={{color: "white"}}>
                         <Button variant={"outline-light"} className={styles.button}>Админ панель</Button>
